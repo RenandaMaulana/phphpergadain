ec233239a11b7ad02860ff82557d4e7fcf024188
RenandaMaulana <133847940+RenandaMaulana@users.noreply.github.com>
2023-09-02 10:13:12 +0700

    Update latihan3.3.php

--- latihan3.3.php
+++ latihan3.3.php
@@ -1,17 +1,39 @@
-<?php    
-$besaran_pinjaman = 1000000;
-$bunga = 0.1;
-$lama_angsuran_bulan = 5;
-$besar_angsuran;
-$total_pinjaman;
-    function totalPinjaman()
-        {
-        $bunga = $besaran_pinjaman * $bunga;
-        $total_pinjaman = $bunga + $besaran_pinjaman;
-        $besar_angsuran = $total_pinjaman / $lama_angsuran_bulan;
-        
-        }
-        
+<?php
+class Hutang {
+    var $besaran_pinjaman;
+    var $besaran_bunga;
+    var $total_pinjaman;
+    var $lama_angsuran;
+    var $besar_angsuran;
 
-echo "Total pinjaman adalah " .$besar_angsuran->totalPinjaman();
+    public function __construct($besaran_pinjaman, $besaran_bunga, $lama_angsuran) {
+        $this->besaran_pinjaman = $besaran_pinjaman;
+        $this->besaran_bunga = $besaran_bunga;
+        $this->lama_angsuran = $lama_angsuran;
+    }
+
+    public function hitungTotalPinjaman() {
+        $this->total_pinjaman = $this->besaran_pinjaman + ($this->besaran_pinjaman * ($this->besaran_bunga / 100));
+        return $this->total_pinjaman;
+    }
+
+    public function hitungBesaranAngsuran() {
+        $this->besar_angsuran = $this->total_pinjaman / $this->lama_angsuran;
+        return $this->besar_angsuran;
+    }
+
+    public function tampilkanInfo() {
+        echo "Besaran pinjaman: Rp " . number_format($this->besaran_pinjaman, 0, ",", ".") . "<br>";
+        echo "Besar bunga: " . $this->besaran_bunga . "%<br>";
+        echo "Total pinjaman: Rp " . number_format($this->total_pinjaman, 0, ",", ".") . "<br>";
+        echo "Lama angsuran: " . $this->lama_angsuran . " bulan<br>";
+        echo "Besar angsuran per bulan: Rp " . number_format($this->besar_angsuran, 0, ",", ".");
+    }
+}
+
+// Penggunaan class Hutang
+$hutang = new Hutang(1000000, 10, 5);
+$hutang->hitungTotalPinjaman();
+$hutang->hitungBesaranAngsuran();
+$hutang->tampilkanInfo();
 ?>
